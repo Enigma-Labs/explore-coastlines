f2baa5af42a0ad360b04387598e9447abfa62a52
Patrick Corbett <p@enigmalabs.io>
2022-01-28 17:14:39 -0800

    fix: setup colab via clone

--- scripts/setup-colab.sh
+++ scripts/setup-colab.sh
@@ -3,13 +3,11 @@ set -ex
 TMP=$(mktemp -d)
 cd "${TMP}" || exit
 
-wget https://raw.githubusercontent.com/Enigma-Labs/explore-coastlines/main/requirements.txt
-wget https://raw.githubusercontent.com/Enigma-Labs/explore-coastlines/main/coastline_incidents.graphql
-wget https://raw.githubusercontent.com/Enigma-Labs/explore-coastlines/main/scripts/get-data.sh
+git clone https://github.com/Enigma-Labs/explore-coastlines.git .
 
 mv "${TMP}"/* /content/
 cd /content/ || exit
 
-chmod +x get-data.sh && ./get-data.sh
+./scripts/get-data.sh
 apt update && apt install virtualenv && virtualenv -p $(which python3) .venv
 pip3 install -r requirements.txt
