dc31eb18c4e82f2a4dc6d803ab99bb8affaa1e34
Patrick Corbett <p@enigmalabs.io>
2022-01-28 17:25:56 -0800

    dev: only copy root git files  to content

--- scripts/setup-colab.sh
+++ scripts/setup-colab.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
+
 set -ex
 TMP=$(mktemp -d)
 cd "${TMP}" || exit
 
 git clone https://github.com/Enigma-Labs/explore-coastlines.git .
 
-mv "${TMP}"/* /content/
+find . -depth 1 -type f | xargs -I _ mv _ /content/
 cd /content/ || exit
 
 ./scripts/get-data.sh
-apt update && apt install virtualenv && virtualenv -p $(which python3) .venv
 pip3 install -r requirements.txt
