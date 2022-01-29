0e7eadea134bda2ad3e98be874ee7f40ad26a13e
Patrick Corbett <p@enigmalabs.io>
2022-01-28 17:55:43 -0800

    dev: final colab adjustments

--- scripts/setup-colab.sh
+++ scripts/setup-colab.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
-REPOSITORY=$PWD/repo
-rm -rf "$REPOSITORY"
+CONTENT=/content
+REPOSITORY=${CONTENT}/repo
+rm -rf $REPOSITORY $CONTENT
 
-git clone https://github.com/Enigma-Labs/explore-coastlines.git "${REPOSITORY}"
+git clone https://github.com/Enigma-Labs/explore-coastlines.git "$REPOSITORY"
 
 ./repo/scripts/get-data.sh
+
 mv -f "$REPOSITORY"/* /content/
+
 pip3 install -r requirements.txt
