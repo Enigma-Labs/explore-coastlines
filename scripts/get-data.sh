04f482779930216ac9facabcc7c854bc0747b2da
Isaac Lee <i@enigmalabs.io>
2022-02-09 17:29:26 +0000

    feat: update scripts

--- scripts/get-data.sh
+++ scripts/get-data.sh
@@ -2,6 +2,7 @@
 
 if [ ! -d "${PWD}/data" ]; then
   mkdir data
-  curl -o data/tl_2021_us_coastline.zip https://www2.census.gov/geo/tiger/TIGER2021/COASTLINE/tl_2021_us_coastline.zip
-  cd data && unzip tl_2021_us_coastline.zip
 fi
+ 
+curl -o data/tl_2021_us_coastline.zip https://www2.census.gov/geo/tiger/TIGER2021/COASTLINE/tl_2021_us_coastline.zip
+cd data && unzip tl_2021_us_coastline.zip
